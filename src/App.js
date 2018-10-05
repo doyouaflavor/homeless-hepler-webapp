4ab0381ec5f91795f8c9c7a8a8f23aabf2845b8b
Chia-Wei Li <dreamcwli@gmail.com>
2018-10-06 02:55:11 +0800

    Implement auth mechanism

--- src/App.js
+++ src/App.js
@@ -4,24 +4,48 @@ import Home from './component/Home';
 import Form from './component/Form';
 import Admin from './component/Admin';
 import Footer from './component/Footer';
+import { AuthContext } from './context';
 
 import ga from './googleAnalytics';
+
+const PROFILE_KEY = 'profile';
 
 const logPageView = () => {
   ga.navigate(window.location.pathname);
 
   return null;
-}
+};
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    const profile = localStorage.getItem(PROFILE_KEY);
+    this.setProfile = this.setProfile.bind(this);
+    this.state = {
+      profile: profile ? JSON.parse(profile) : null,
+      setProfile: this.setProfile,
+    };
+  }
 
   componentWillMount() {
     ga.initialize()
   }
 
+  setProfile(profile) {
+    this.setState({
+      profile,
+    });
+    if (profile) {
+      localStorage.setItem(PROFILE_KEY, JSON.stringify(profile));
+    } else {
+      localStorage.removeItem(PROFILE_KEY);
+    }
+  }
+
   render() {
     return (
-      <div>
+      <AuthContext.Provider value={this.state}>
         <Route path="/" component={logPageView} />
         <Switch>
           <Route exact path="/" component={Home} />
@@ -29,7 +53,7 @@ class App extends Component {
           <Route path="/admin" component={Admin} />
         </Switch>
         <Footer/>
-      </div>
+      </AuthContext.Provider>
     );
   }
 }
