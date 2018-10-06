de99213543f347e7741e1b7e63b5f69462d050fe
Chia-Wei Li <dreamcwli@gmail.com>
2018-10-06 14:59:00 +0800

    Add token header to request if available

--- src/api/request.js
+++ src/api/request.js
@@ -1,13 +1,31 @@
 import axios from 'axios';
 
+import { getProfile } from '../utils';
+
 const API_ROOT = 'https://homeless-helper-api.doyouaflavor.tw/api/v1';
 
 async function get(url) {
-  return await axios.get(`${API_ROOT}${url}`)
+  const profile = getProfile();
+  const headers = profile
+    ? {
+      'x-access-token': profile.token,
+    }
+    : {};
+  return await axios.get(`${API_ROOT}${url}`, {
+    headers,
+  });
 }
 
 async function post(url, data) {
-  return await axios.post(`${API_ROOT}${url}`, data)
+  const profile = getProfile();
+  const headers = profile
+    ? {
+      'x-access-token': profile.token,
+    }
+    : {};
+  return await axios.post(`${API_ROOT}${url}`, data, {
+    headers,
+  });
 }
 
 export default {
